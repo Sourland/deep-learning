87d1b147e837f4c4c23327b77fc50362b9134b2b
Dimitrios Sourlantzis <jem2010@windowslive.com>
2022-11-26 15:00:56 +0200

    add one hot encoding

--- src/data.jl
+++ src/data.jl
@@ -1,5 +1,4 @@
 using MLDatasets:CIFAR10
-using Images, ImageInTerminal
 x_train, y_train = CIFAR10.traindata();
 x_test, y_test = CIFAR10.testdata();
 x_train = 0.299 * x_train[:,:,1,:] + 0.587* x_train[:,:,2,:] + 0.114 * x_train[:,:,3,:]
@@ -9,3 +8,13 @@ x_test = reshape(x_test, (32*32,size(x_test)[3]))
 
 
 
+function one_hot_encoding!( number_of_classes, labels)
+    encoded_labels = zeros(number_of_classes, size(labels)[1])
+    for idx in 1:size(labels)[1]
+        encoded_labels[labels[idx]+1,idx] = 1
+    end
+    return encoded_labels
+end
+
+y_train = one_hot_encoding!(maximum(y_train)+1, y_train)
+y_test = one_hot_encoding!(maximum(y_test)+1, y_test)
