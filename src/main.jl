13a2321c4f7af0184059a029153a746f6a476d75
Dimitrios Sourlantzis <jem2010@windowslive.com>
2022-11-27 22:15:47 +0200

    add main

--- src/main.jl
+++ src/main.jl
@@ -4,7 +4,7 @@ include("train.jl")
 include("utility_functions.jl")
 
 using MLDatasets:MNIST
-using EvalMetrics
+using MLBase
 x_train, y_train = MNIST.traindata();
 x_test, y_test = MNIST.testdata();
 
@@ -17,9 +17,16 @@ net_layers = [
             Dense("hidden_layer_1", m*n, 256, ReLU!) 
             Dense("hidden_layer_2",256, 128, ReLU!) 
             Dense("output_layer",128, 10, softmax!)
-            ]
+            ];
 
-net_layers, metrics = train(net_layers, x_train, y_train, 10, 256, 0.05);
+net_layers, metrics = train(net_layers, x_train, y_train, 100, 256, 0.05);
 
-layers, outputs, ŷ = forward(net_layers, x_test)
-confusion_matrix = ConfusionMatrix(y_test, predict(ŷ).-1)
+layers, outputs, ŷ = forward(net_layers, x_test);
+confusion_matrix = confusmat(10, y_test.+1, predict(ŷ))
+ROC = roc(y_test, predict(ŷ).-1)
+
+model_recall = recall(ROC);
+model_precision = precision(ROC)
+model_f1_score = f1score(ROC)
+
+print("RECALL: " * string(model_recall) * " | PRECISION: " * string(model_recall) * " | F1: " * string(model_f1_score))
